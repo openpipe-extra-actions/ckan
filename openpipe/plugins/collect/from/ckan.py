8a24ec1b9c5f62e34c2e245f365d508118efc38b
João Pinto <lamego.pinto@gmail.com>
2019-02-12 14:19:23 +0000

    Added the ignore_not_found option

--- openpipe/plugins/collect/from/ckan.py
+++ openpipe/plugins/collect/from/ckan.py
@@ -2,10 +2,14 @@
 # ckan
 """
 from openpipe.engine import PluginRuntime
-from ckanapi import RemoteCKAN
+from ckanapi import RemoteCKAN, errors
 
 
 class Plugin(PluginRuntime):
+
+    __default_config__ = {
+        "ignore_not_found": False
+    }
 
     def on_start(self, config):
         self.ckan = RemoteCKAN(config['url'])
@@ -13,7 +17,13 @@ class Plugin(PluginRuntime):
     def on_input(self, item):
         action = self.config['action']
         args = self.config.get('arguments', {})
-        data = self.ckan.call_action(action, args)
+        try:
+            data = self.ckan.call_action(action, args)
+        except errors.NotFound:
+            if self.config['ignore_not_found']:
+                return
+            else:
+                raise
         if isinstance(data, list):
             for new_item in data:
                 self.put(new_item)
